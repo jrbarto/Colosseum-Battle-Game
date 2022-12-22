b8bae83b295d1c0c710776e8f30bacfe6b4f07d0
jrbarto <bartorobjeff@gmail.com>
2022-12-22 15:17:17 -0500

    Cap hp bar length

--- Assets/Scripts/PlayerHealthController.cs
+++ Assets/Scripts/PlayerHealthController.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class PlayerHealthController : HealthController
 {
+    public Texture2D boxBorder;
     private Camera mainCam;
 
     void Start()
@@ -11,24 +12,30 @@ public class PlayerHealthController : HealthController
 
     void OnGUI() {
         int cursorSize = 20;
-        float cursorX = mainCam.pixelWidth / 2 - cursorSize / 4;
-        float cursorY = mainCam.pixelHeight / 2 - cursorSize / 2;
+        int cursorX = mainCam.pixelWidth / 2 - cursorSize / 4;
+        int cursorY = mainCam.pixelHeight / 2 - cursorSize / 2;
         GUI.Label(new Rect(cursorX, cursorY, cursorSize, cursorSize), "+");
-        float healthY = mainCam.pixelHeight - 40;
-        float healthX = 30;
+        int healthY = mainCam.pixelHeight - 40;
+        int healthX = 30;
+        int boxHeight = 20 + mainCam.pixelHeight / 70;
+        float hpLengthCap = 20.0f;
         Rect healthBox;
         string healthText;
         if (healthPoints <= 0) {
-            healthBox = new Rect(healthX, healthY, 500, mainCam.pixelHeight / 20);
+            healthBox = new Rect(healthX, healthY, mainCam.pixelWidth - healthX * 2, boxHeight);
             healthText = "YOU ARE DEAD";
         } else {
-            healthBox = new Rect(healthX, healthY, healthPoints * 100, mainCam.pixelHeight / 20);
-            healthText = healthPoints.ToString();
+            float boxWidth = ((mainCam.pixelWidth - healthX * 2) / ((float)maxHealthPoints  / (float)healthPoints)) * (maxHealthPoints / (hpLengthCap + (maxHealthPoints - hpLengthCap > 0 ? maxHealthPoints - hpLengthCap : 0)));
+            healthBox = new Rect(healthX, healthY, boxWidth, boxHeight);
+            healthText = $"{healthPoints.ToString()} / {maxHealthPoints.ToString()}";
         }
         GUI.DrawTexture(healthBox, Texture2D.redTexture, ScaleMode.StretchToFill, false);
         GUIStyle boxStyle = new GUIStyle();
         boxStyle.alignment = TextAnchor.MiddleCenter;
+        boxStyle.fontSize = mainCam.pixelHeight / 30;
         boxStyle.normal.textColor = Color.white;
+        boxStyle.border = new RectOffset(2, 2, 2, 2);
+        boxStyle.normal.background = boxBorder;
         GUI.Box(healthBox, healthText, boxStyle);
     }
 }
