557f3e95e33d48b96dd932e06b7da4e4f71bbc44
jrbarto <bartorobjeff@gmail.com>
2023-01-21 14:18:52 -0500

    Add stamina reload radial

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.UI;
 
 public class PlayerMovementController : MonoBehaviour
@@ -17,6 +18,7 @@ public class PlayerMovementController : MonoBehaviour
     private int oldStaminaPoints;
     private RectTransform staminaBar;
     private Transform staminaRadialTransform;
+    private GameObject[] staminaRadialSegments;
     private GameObject staminaText;
     private float maxStaminaBarLength;
     private float maxStaminaBarPoints = 20;
@@ -30,11 +32,38 @@ public class PlayerMovementController : MonoBehaviour
         staminaText = staminaBarTransform.Find("Stamina Text").gameObject;
         maxStaminaBarLength = staminaBar.rect.width;
         staminaRadialTransform = uiCanvas.transform.Find("Stamina Radial");
+        List<GameObject> transitiveSegments = new List<GameObject>();
+        for (int i = 0; i < staminaRadialTransform.childCount; i++) {
+            transitiveSegments.Add(staminaRadialTransform.GetChild(i).gameObject);
+        }
+        staminaRadialSegments = transitiveSegments.ToArray();
     }
 
     void Update()
     {
         bool alive = gameObject.GetComponent<PlayerCombatController>().alive;
+        if (staminaPoints == 0 || !alive) {
+            staminaPoints = 0;
+            staminaText.GetComponent<Text>().text = "";
+        } else {
+            staminaText.GetComponent<Text>().text = $"{staminaPoints.ToString()} / {maxStaminaPoints.ToString()}";
+        }
+        if (staminaPoints != oldStaminaPoints) {
+            if (maxStaminaPoints > maxStaminaBarPoints) {
+                maxStaminaBarPoints = maxStaminaPoints;
+            }
+            float staminaBarRatio = staminaPoints / maxStaminaBarPoints;
+            float currentLength = staminaBar.rect.width;
+            staminaBar.sizeDelta = new Vector2(maxStaminaBarLength * staminaBarRatio, staminaBar.rect.height);
+            staminaBar.localPosition -= new Vector3((currentLength - staminaBar.rect.width) / 2, 0, 0);
+            staminaRadialTransform.localPosition = new Vector3(
+                staminaBar.localPosition.x + (staminaBar.sizeDelta.x / 2) + 10, 
+                staminaRadialTransform.localPosition.y,
+                staminaRadialTransform.localPosition.z
+            );
+            oldStaminaPoints = staminaPoints;
+        }
+        rotateRadial(alive);
         if (!alive) {
             return;
         }
@@ -53,7 +82,11 @@ public class PlayerMovementController : MonoBehaviour
 
         if (dashVector != null) {
             movement = (Vector3)dashVector;
-        } else if (staminaPoints >= 1 && Input.GetKeyDown(KeyCode.LeftShift) ) {
+        } else if (
+            staminaPoints >= 1 && 
+            Input.GetKeyDown(KeyCode.LeftShift) &&
+            (movement.x != 0 || movement.z != 0)
+        ) {
             staminaPoints -= 1;
             StartCoroutine(dash(movement));
         } else {
@@ -62,27 +95,26 @@ public class PlayerMovementController : MonoBehaviour
 
         movement = transform.TransformDirection(movement);
         controller.Move(movement);
+    }
 
-        if (staminaPoints == 0 || !alive) {
-            staminaText.GetComponent<Text>().text = "";
-        } else {
-            staminaText.GetComponent<Text>().text = $"{staminaPoints.ToString()} / {maxStaminaPoints.ToString()}";
-        }
-        if (staminaPoints != oldStaminaPoints) {
-            if (maxStaminaPoints > maxStaminaBarPoints) {
-                maxStaminaBarPoints = maxStaminaPoints;
+    private void rotateRadial(bool alive) {
+        if (staminaRegenTimer != null && alive) {
+            float fillRatio = (float)(staminaRegenRate - staminaRegenTimer) / staminaRegenRate;
+            for (int i = 0; i < Mathf.Floor(staminaRadialSegments.Length * fillRatio); i++) {
+                GameObject segment = staminaRadialSegments[i];
+                if (fillRatio >= 1) {
+                    segment.SetActive(false);
+                } else {
+                    segment.SetActive(true);
+                }
             }
-            float staminaBarRatio = staminaPoints / maxStaminaBarPoints;
-            float currentLength = staminaBar.rect.width;
-            staminaBar.sizeDelta = new Vector2(maxStaminaBarLength * staminaBarRatio, staminaBar.rect.height);
-            staminaBar.localPosition -= new Vector3((currentLength - staminaBar.rect.width) / 2, 0, 0);
-            staminaRadialTransform.localPosition = new Vector3(
-                staminaBar.localPosition.x + (staminaBar.sizeDelta.x / 2) + 10, 
-                staminaRadialTransform.localPosition.y, 
-                staminaRadialTransform.localPosition.z
-            );
-            oldStaminaPoints = staminaPoints;
+        } else {
+            for (int i = 0; i < staminaRadialSegments.Length; i++) {
+                GameObject segment = staminaRadialSegments[i];
+                segment.SetActive(false);
+            }
         }
+
     }
 
     IEnumerator dash(Vector3 movement) {
