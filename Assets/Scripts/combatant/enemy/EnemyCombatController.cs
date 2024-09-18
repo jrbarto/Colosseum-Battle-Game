fdda0e6fbe0d6318fd64c3bcbbec9d0c4d8923e3
jrbarto <bartorobjeff@gmail.com>
2024-09-17 22:20:13 -0400

    add enemy running transitions

--- Assets/Scripts/combatant/enemy/EnemyCombatController.cs
+++ Assets/Scripts/combatant/enemy/EnemyCombatController.cs
@@ -7,6 +7,7 @@ public class EnemyCombatController : CombatController
     public Transform enemyCenter;
     private GameObject player;
     private NavMeshAgent navAgent;
+    private float currentSpeed;
 
     void Start() {
         player = GameObject.FindGameObjectsWithTag("Player")[0];
@@ -15,12 +16,12 @@ public class EnemyCombatController : CombatController
     }
 
     // debug function to view stopping distance when enemy is selected in scene mode during play
-    void OnDrawGizmosSelected() {
-        navAgent = GetComponent<NavMeshAgent>();
-        Gizmos.color = Color.red;
-        // draw a wire sphere at the enemy's position with the radius of stoppingDistance
-        Gizmos.DrawWireSphere(transform.position, navAgent.stoppingDistance);
-    }
+    // void OnDrawGizmosSelected() {
+    //     navAgent = GetComponent<NavMeshAgent>();
+    //     Gizmos.color = Color.red;
+    //     // draw a wire sphere at the enemy's position with the radius of stoppingDistance
+    //     Gizmos.DrawWireSphere(transform.position, navAgent.stoppingDistance);
+    // }
 
     void Update()
     {
@@ -47,6 +48,26 @@ public class EnemyCombatController : CombatController
             animator.SetBool("walking", true);
             animator.SetBool("attacking", false);
         }
+
+        // TODO: fix this to have a running animation that plays when movespeed is high
+        // TODO: also cap the animation speed so it doesn't look so dumb
+        float moveSpeed = navAgent.velocity.magnitude / 3;
+        Debug.Log("SETTING MOVE SPEED WITH " + Mathf.Clamp(moveSpeed, 0, 2.0f));
+        animator.SetFloat("moveSpeed", Mathf.Clamp(moveSpeed, 1.0f, 2.0f));
+
+        float targetSpeed = navAgent.desiredVelocity.magnitude;
+
+        // Smoothly adjust the agent's speed
+        if (currentSpeed < targetSpeed) {
+            // Accelerate up to target speed
+            currentSpeed += 2.0f * Time.deltaTime;
+            currentSpeed = Mathf.Min(currentSpeed, targetSpeed); // cap the speed at target
+        } else if (currentSpeed > targetSpeed) {
+            // decelerate to target speed immediately (otherwise he slides past the player)
+            currentSpeed = targetSpeed; // snap to targetSpeed for instant stop
+        }
+
+        navAgent.velocity = navAgent.desiredVelocity.normalized * currentSpeed;
 
         float chaseAngle = 5.0f;
         Vector3 targetDir = (player.transform.position - transform.position).normalized;
--- Assets/Scripts/combatant/enemy/IndependentRotationController.cs
+++ Assets/Scripts/combatant/enemy/IndependentRotationController.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 
+// used so the rotation caused by animations doesn't cause enemy spherecast to be off
+// the spherecase uses the orientation fo the enemy's center
 public class IndependentRotationController : MonoBehaviour
 {
     public Transform trackedObject;
